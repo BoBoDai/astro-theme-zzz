f4e95b6aa028b4494badc37c79447c4115985cf3
bobo <bobodai515@163.com>
2024-12-29 12:21:07 +0800

    fix: add base for resocuse

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	// Demo page
 	site: 'https://bobodai.github.io/astro-theme-zzz/',
+	// Demo base
+	base: 'astro-theme-zzz',
 	integrations: [mdx(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import config from '../astro.config.mjs';
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const NAME = 'bobo';
+export const BASE = config.base;
+export const HEADER_IMG = '/' + BASE + '/' + 'IMG_0073.JPG';
+export const FIRST_ACTIVE = '/' + BASE + '/' + 'first_active.svg';
+export const LAST_ACTIVE = '/' + BASE + '/' + 'last_active.svg';
+export const MID_ACTIVE = '/' + BASE + '/' + 'mid_active.svg';
+export const FAVICON = '/' + BASE + '/' + 'favicon.svg';
